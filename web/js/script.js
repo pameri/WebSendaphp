e0d0da1b9c15dc3cf34f257ce5e932d423516417
percy ameri <percy_ameri@yahoo.es>
2011-12-13 21:27:27 -0500

    subiendo todo los files2

---
+++ web/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$(".carousel").dualSlider({
+		auto:true,
+		autoDelay: 8000,
+		easingCarousel: "easeInOutBack",
+		easingDetails: "easeInOutExpo",
+		durationCarousel: 800,
+		durationDetails: 500
+	});
+
+});
